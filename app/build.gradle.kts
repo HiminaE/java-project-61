5bf7578e92a694f42ff9f731c43b9400198116f2
helen <HiminaEV@yandex.ru>
2024-02-25 18:56:08 +0300

    application added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,9 @@ plugins {
 application {
     mainClass.set("hexlet.code.App")
 }
-
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String input = scanner.nextLine();
+        System.out.println("Hello, " + input);
+
     }
 }
