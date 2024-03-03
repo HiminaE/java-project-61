8f726d8424a34e7dc661d8016e915b348f291a75
helen <HiminaEV@yandex.ru>
2024-03-03 17:58:00 +0300

    add maintainability badge

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,11 +4,33 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("May I have your name?");
-        String input = scanner.nextLine();
-        System.out.println("Hello, " + input);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int input = scanner.nextInt();
+        if (input == 1) {
+            System.out.println();
+            System.out.println("Welcome to the Brain Games!");
+
+            System.out.println("May I have your name?");
+            String name = scanner.nextLine();
+            System.out.println("Hello, " + name);
+        }
+
+//        make run-dist
+//        Please enter the game number and press Enter.
+//        1 - Greet
+//        0 - Exit
+//        Your choice: 1
+//
+//        Welcome to the Brain Games!
+//                May I have your name? John
+//        Hello, John!
+
+
 
     }
 }
