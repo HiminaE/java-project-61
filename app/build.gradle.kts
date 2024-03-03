2d215fb1c283128db2767f2c41f00241bd887464
helen <HiminaEV@yandex.ru>
2024-03-03 17:56:49 +0300

    add maintainability badge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     application
+    checkstyle
     id("java")
 }
 
 application {
     mainClass.set("hexlet.code.App")
 }
+
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
 }
