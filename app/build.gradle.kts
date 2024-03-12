0d4a244d6def1811edf7a7c0de9f83ae7c372f63
helen <HiminaEV@yandex.ru>
2024-03-12 13:53:52 +0300

    add maintainability badge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,11 +17,4 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
 
-tasks.test {
-    useJUnitPlatform()
-}
